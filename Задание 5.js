13ad704cd75de93e060ceb881eca268e4052ec93
chloetorch <125360014+chloetorch@users.noreply.github.com>
2023-04-21 18:16:40 +0300

    Update Задание 5.js

--- Задание 5.js
+++ Задание 5.js
@@ -3,55 +3,56 @@ class Electric {
       this.name = name,
          this.power = power,
          this.color = color,
-         this.enabled = false;
+         this.enabled = false
    }
    getPower() {
-      function () {
-         return this.enabled ? this.power : 0;
-      };
+      return this.enabled ? this.power : 0;
+   }
+   toggleEnable() {
+      this.enabled = !this.enabled
+   }
+
+   getInfo() {
+      return `Устройство ${this.name} - ${this.enabled ? 'включено' : 'выключено'}, текущее потребление: ${this.getPower()} W`
    }
 }
 
 class Comp extends Electric {
-   constructor(processor, power, isEnabled, enabled, name) {
-      super(name);
-      this.processor = processor;
-      this.power = power,
-         this.isEnabled = isEnabled;
-      this.enabled = isEnabled ? true : false;
-   }
-   getPower() {
-      function sum(arr) {
-         let result = 0
-         arr.forEach((item) => {
-            result += item.getPower()
-         })
-         return result;
-      }
+   constructor(processor, power, name) {
+      super(name, power),
+         this.processor = processor,
+         this.power = power,
+         this.name = name
    }
 }
 
 class Lamp extends Electric {
-   constructor(bright, power, isEnabled, enabled, name) {
-      super(name);
-      this.bright = bright;
-      this.power = power,
-         this.isEnabled = isEnabled;
-      this.enabled = isEnabled ? true : false;
-   }
-   getPower() {
-      function sum(arr) {
-         let result = 0
-         arr.forEach((item) => {
-            result += item.getPower()
-         })
-         return result;
-      }
+   constructor(bright, power, name) {
+      super(name, power),
+         this.bright = bright,
+         this.power = power,
+         this.name = name
    }
 }
 
-const comp = new Comp(4, 200, true, true, 'Comp');
-console.log(comp.getPower(sum([comp, lamp])))
+function sum(arr) {
+   let result = 0
+   arr.forEach((item) => {
+      result += item.getPower()
+   })
+   return result;
+}
 
-const lamp = new Lamp('bright', 80, true, true, 'Lamp');
-console.log(lamp.getPower(sum([comp, lamp])))
+const comp = new Comp(4, 200, 'Computer');
+const lamp = new Lamp('bright', 80, 'Lamp');
+
+console.log(comp.getInfo())
+comp.toggleEnable()
+console.log(comp.getInfo())
+
+console.log(lamp.getInfo())
+lamp.toggleEnable()
+console.log(lamp.getInfo())
+
+
+console.log(`Общее потребление электроприборов ${sum([comp, lamp])} w`)
